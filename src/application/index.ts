93bb2b92a131456ffc8f8bff136b505cd15dca90
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-13 20:25:57 -0400

    Create TokenService

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,3 +1,4 @@
 export * from './protocol/password_hasher';
+export * from './protocol/token_service';
 export * from './repository/user_repository';
 export * from './errors';
---
+++ src/application/protocol/token_service.ts
@@ -0,0 +1,35 @@
+/**
+ * Resultado do token de acesso
+ */
+export interface TokenResult {
+  /**
+   * Token de acesso
+   */
+  token: string;
+
+  /**
+   * Expiração do token em unix epoch
+   */
+  expire_in: string;
+}
+
+/**
+ * Serviço de token
+ */
+export abstract class TokenService {
+  /**
+   * Gerar token de acesso
+   *
+   * @param payload Carga útil
+   */
+  abstract generate<T>(payload: T): Promise<TokenResult>;
+
+  /**
+   * Validar o token
+   *
+   * Se estiver inválido retorna null
+   *
+   * @returns Carga útil ou null
+   */
+  abstract validate<T>(token: string): Promise<null | T>;
+}
