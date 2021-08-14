04210ddd2f400e7dad8c45569746e050395e33f7
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-14 19:50:48 -0400

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/app.ts',
+    '<rootDir>/src/application/error.ts',
+  ],
+
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
 };
