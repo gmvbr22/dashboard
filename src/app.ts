349b9dd4b7260d073fdfdebd9d6896189502bcb8
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-13 20:14:30 -0400

    Create Container and bind PasswordHasher

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
-/**
- * Get message
- */
-export function message() {
-  return 'Hello world';
+import {Container} from 'inversify';
+import {PasswordHasher} from './application';
+import {BcryptAdapter} from './infrastructure';
+
+async function main() {
+  const container = new Container();
+  container.bind(PasswordHasher).toConstantValue(new BcryptAdapter(11));
 }
+
+main();
