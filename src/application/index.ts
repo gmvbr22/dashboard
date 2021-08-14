38b300dea6a9375f1aba2cdd34f99343eab70b4b
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-13 20:33:16 -0400

    Update application index

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,5 +1,6 @@
+export * from './protocol/captcha_service';
+export * from './protocol/locale_service';
 export * from './protocol/password_hasher';
 export * from './protocol/token_service';
-export * from './protocol/locale_service';
 export * from './repository/user_repository';
 export * from './errors';
