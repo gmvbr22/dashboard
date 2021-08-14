c927b2792fbcc82a2de3d4cffc4dedb25e8c524c
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-08-13 20:04:04 -0400

    Update User entity

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,21 +1,7 @@
 export class User {
-  private id?: string;
-  private name?: string;
-  private email?: string;
-  private password?: string;
-
-  public setEmailPassword(email?: string, password?: string): User {
-    this.email = email;
-    this.password = password;
-    return this;
-  }
-
-  public toJson() {
-    return {
-      id: this.id,
-      name: this.name,
-      email: this.email,
-      password: this.password,
-    };
-  }
+  public id?: string;
+  public name?: string;
+  public email?: string;
+  public password?: string;
+  public role?: string;
 }
